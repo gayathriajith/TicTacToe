4448b560cd44cd9140bbacecbdb337607f925f9e
gayathriajith <38522244+gayathriajith@users.noreply.github.com>
2018-04-25 20:22:05 +0530

    Tic Tac Toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,6 @@
+
 print('Welcome to Tic Tac Toe!')
+
 
 from IPython.display import clear_output
 import sys
@@ -13,7 +15,7 @@ def space_check(board, position):
         empty=False
     return empty
     
-def win_check(board, mark):
+def win_check(board,mark):
         
     
     for i in range (1,8,3):
@@ -81,16 +83,16 @@ def replay(marker,board):
             print("\nTYPE Y to keep playing, N to stop playing!! ")
             replay(marker,board)
     
-        if play==True and marker=='\033[1m\033[91mX\033[0m':
+        if play==True and marker=='X':
             playerY(board)
-        if play==True and marker=='\033[1m\033[92mO\033[0m':
+        if play==True and marker=='O':
             playerX(board)
           
     
         
         
 def display_board(board):
-    clear_output()
+    print('\n'*100)
     print(f"-----|-----|-----")
     print(f"     |     |     ")
     print(f"  {board[7]}  |  {board[8]}  |  {board[9]}  ")    
@@ -104,19 +106,19 @@ def display_board(board):
     print(f"  {board[1]}  |  {board[2]}  |  {board[3]}  ")
     print(f"     |     |     ")
     print(f"--1--|--2--|--3--")
-    
+    return
 
 
        
     
 def playerX(board):
     
-    position=input(" Insert '\033[1m\033[91mX\033[0m' in : [Choose from position 1 to 9]")
+    position=input(" Insert 'X' in : [Choose from position 1 to 9]")
     pos= int(position)
     if(pos not in range(0,10)):
         print("\n\nSorry! You entered wrong position!\n CHOOSE FROM POSITION 1 to 9 ")
         playerX(board)
-    marker = '\033[1m\033[91mX\033[0m'
+    marker = 'X'
     if(space_check(board, pos)==True):   
         
         place_marker(board, marker, pos)
@@ -128,12 +130,12 @@ def playerX(board):
     
 def playerY(board):
     
-    position = input(" Insert '\033[1m\033[92mO\033[0m' in : [Choose from position 1 to 9]")
+    position = input(" Insert 'O' in : [Choose from position 1 to 9]")
     pos= int(position)
     if(pos not in range(0,10)):
         print("\n\nSorry! You entered wrong position!\n CHOOSE FROM POSITION 1 to 9 ")
         playerY(board)
-    marker='\033[1m\033[92mO\033[0m'
+    marker='O'
     if(space_check(board, pos)==True):   
         
         place_marker(board, marker, pos)
@@ -154,25 +156,24 @@ def place_marker(board,marker, position):
     replay(marker,board)
       
 
-    
 def start_game():
     
     test_board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']  
     play=None
     empty=None
     display_board(test_board)
-    player1 = input("\tPlease pick a marker '\033[1m\033[91mX\033[0m' or '\033[1m\033[92mO\033[0m'")
+    player1 = input("\tPlease pick a marker 'X' or 'O'")
     if(player1=='X'or player1=='x'):
-        print("\nPlayer 1 has selected '\033[1m\033[91mX\033[0m'")
-        print("\nPlayer 2 has selected '\033[1m\033[92mO\033[0m'")
+        print("\nPlayer 1 has selected 'X'")
+        print("\nPlayer 2 has selected 'O'")
         print("\nPlayer 1 gets the first chance")
         playerX(test_board)
         
         
     if(player1=='O'or player1=='o'):
-        print("\nPlayer 1 has selected '\033[1m\033[92mO\033[0m'")
-        print("\nPlayer 2 has selected '\033[1m\033[91mX\033[0m'")
+        print("\nPlayer 1 has selected 'O'")
+        print("\nPlayer 2 has selected 'X'")
         print("\nPlayer 1 gets the first chance")
         playerY(test_board)
  
-
+start_game()
